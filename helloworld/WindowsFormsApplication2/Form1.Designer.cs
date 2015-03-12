dbba154128cb967dc0844830c845a4f18b24b0fc
Huỳnh Thị Hồng <huynhhongk46thkt@gmail.com>
2015-03-13 00:42:18 +0700

    up baitap hello world

---
+++ helloworld/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hello = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hello
+            // 
+            this.hello.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.hello.AutoSize = true;
+            this.hello.Font = new System.Drawing.Font(".VnKoala", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hello.Location = new System.Drawing.Point(43, 79);
+            this.hello.Name = "hello";
+            this.hello.Size = new System.Drawing.Size(215, 55);
+            this.hello.TabIndex = 0;
+            this.hello.Text = "HelloWorld";
+            this.hello.Click += new System.EventHandler(this.hello_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(105, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Chao";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hello;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
